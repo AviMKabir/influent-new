8d54759cd09917ae1710fb95b2e47a3e7ac388c3
Avi M Kabir <avimishkakabir@gmail.com>
2019-11-18 00:52:47 -0500

    added require path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+var path = require('path');
 
 const users = require('./routes/api/users');
 
