44343cc9284965ae4b02be3dded9da6eb22ba2ca
Avi M Kabir <avimishkakabir@gmail.com>
2019-11-19 18:41:42 -0500

    fron tpage responsiveness added

--- client/src/pages/home1/index.js
+++ client/src/pages/home1/index.js
@@ -2,7 +2,11 @@ import React from "react";
 
 import Logo from "../img/influent-logo-vector-2.svg";
 // eslint-disable-next-line 
-import Hero from "../img/hero-notext.png"
+import Hero from "../img/hero-notext.png";
+// eslint-disable-next-line 
+import Hero2 from "../img/influencer-why-1.png";
+// eslint-disable-next-line 
+import Hero3 from "../img/brand-why-1.png";
 import "./style.css";
 import Iphone from "../img/phone_iphone-24px.svg"
 import BarChart from "../img/bar_chart-24px.svg"
@@ -13,29 +17,29 @@ import Cash from "../img/monetization_on-24px.svg"
 function About() {
   return (
     <div>
-​
-{/* navbar */}
+
+      {/* navbar */}
       <div className="navbar ">
         <a className="navbar-brand" href="/">
-          <img src={Logo} width="150" alt="" />
+          <img className="the-logo " src={Logo} width="150px" alt="" />
         </a>
-​
+
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <a className="nav-link sign-in" href="/login"><h4>sign in</h4></a>
           </li>
         </ul>
-​
+
       </div>
-​
-{/* ​hero */}
+
+      {/* ​hero */}
       <div className="earnings clearfix">
         <div className="container">
           <div className="col-sm-12 col-xs-12 earning-vertical-align">
             <div className="earnings-content">
               <h1 className="hero1-text"><span className="erato-italic">take control</span> in the</h1>
               <h1 className="hero1-text">new age of e-commerce</h1>
-​
+
               <a className="btn btn-1" href="/" data-toggle="modal" data-target="#myModal"> get started </a>
             </div>
           </div>
@@ -43,7 +47,7 @@ function About() {
       </div>
 
 
-{/* mission */}
+      {/* mission */}
       <div className="container">
         <div className="row">
           <h3 className="mission erato-italic">our mission</h3>
@@ -52,25 +56,26 @@ function About() {
           {/* our mission paragraph */}
           <p className="wecreated">we created <span className="erato-italic">in</span><span className="erato">fluent</span>  to seamelessy connect brands and influencers. in order to do this, we empower brands to market themselves the way they see fit to prospective influencers, who in turn can view potential brands with which to work. within this propreitary marketplace is where the magic happens, innovative new products get paired with the bleeding edge of social networking personalities, to stay one step ahead in the new age of e-commerce.</p>
         </div>
-       
+
         {/* //container div for icons*/}
       </div>
+
       <div className="container icons">
 
- {/* icon row  */}
- <div className="row ">
+        {/* icon row  */}
+        <div className="row ">
           <div className="col-4 ">
             <img className="iphone" src={Iphone} alt="iphone icon"
             />
             <p className="iconP">connect with both influencers and brands looking to advertise and showcase their products</p>
           </div>
-​
+
           <div className="col-4 ">
             <img className="cash" src={Cash} alt="money icon"
             />
             <p className="iconP">watch your profits grow as followers engage with your advertisement.</p>
           </div>
-​
+
           <div className="col-4 ">
             <img className="barchart" src={BarChart} alt="barchart icon"
             />
@@ -79,92 +84,109 @@ function About() {
         </div>
 
         <div class="row">
-        <div className="col-12">
-          <h2 className="text-center"><span className="erato-italic">why in</span>fluent?</h2>
+          <div className="col-12">
+            <h2 className="text-center"><span className="erato-italic">why in</span>fluent?</h2>
+          </div>
         </div>
-      </div>
 
       </div>
 
 
-​
-​
-    
-​
-​
+
+
+
+
+
       {/* heroes */}
       {/* influencer  */}
-      <div className="row">
-        <div className="col-12">
-​
-​
-          <div className="influencerwhyimg ">
-            <div className="getstarted1">
-              <a href="/registerinf" className="btn startbtn">get started</a>
+
+      <div class="clearfix">
+
+        <div className="earnings-2 ">
+          <div className="container">
+            <div className="col-sm-12 col-xs-12 earning-vertical-align">
+              <div className="earnings-2-content">
+                {/* <a href="/registerinf" className="btn startbtn">get started</a> */}
+                <h1 className="hero2-text">influencers</h1>
+                <h1 className="hero2-text-p">- connect with brands</h1>
+                <h1 className="hero2-text-p">- engage your followers</h1>
+                <h1 className="hero2-text-p">- monetize your online presence</h1>
+                <a href="/registerinf" className="btn startbtn">get started</a>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="col-12">
-          <div className="brandwhyimg">
-            <div className="getstarted2">
-              <a href="/registerbrand" className="btn startbtn">get started</a>
+        <div className="earnings-3 ">
+          <div className="container">
+            <div className="col-sm-12 col-xs-12 earning-vertical-align">
+              <div className="earnings-3-content">
+                {/* <a href="/registerinf" className="btn startbtn">get started</a> */}
+                <h1 className="hero2-text">brands</h1>
+                <h1 className="hero2-text-p">- link with trending personalities</h1>
+                <h1 className="hero2-text-p">- broaden reach to new markets</h1>
+                <h1 className="hero2-text-p">- increase profits</h1>
+                <a href="/registerbrand" className="btn startbtn2">get started</a>
+              </div>
             </div>
           </div>
         </div>
-        
+
+
+
+
+
+
       </div>
-​
-      {/* brand  */}
+
+
+    
+
+
       <div className="row">
-       
-      </div>
-​
-​
-      <div className="row">
-      
-          <img className="foot" src={Logo} alt="" />
-        
+
+        <img className="foot" src={Logo} alt="" />
+
       </div>
 
 
 
-{/* <!-- Modal --> */}
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog modal-dialog-centered">
+      {/* <!-- Modal --> */}
+      <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog modal-dialog-centered">
 
-    {/* <!-- Modal content--> */}
-    <div class="modal-content">
+          {/* <!-- Modal content--> */}
+          <div class="modal-content">
 
-  
-     
-      <div class="modal-body">
-      <div className="">
-                    <div className="">
-                        <div className="">
-                            <div className="text-center">
 
-                                <h1 className="proxima">register</h1>
-                                <p className="lead">are you a brand or an influencer?</p>
-                                <p className="lead">
-                                    <a href="/registerbrand"><button className="btn-submit " >brand</button></a>
-                                    <br></br>
-                                    <a href="/loginbrand"><button className="btn-submit">influencer</button></a>
-                                </p>
-                         
 
-                            </div>
-                        </div>
+            <div class="modal-body">
+              <div className="">
+                <div className="">
+                  <div className="">
+                    <div className="text-center">
+
+                      <h1 className="proxima">register</h1>
+                      <p className="lead">are you a brand or an influencer?</p>
+                      <p className="lead">
+                        <a href="/registerbrand"><button className="btn-submit " >brand</button></a>
+                        <br></br>
+                        <a href="/loginbrand"><button className="btn-submit">influencer</button></a>
+                      </p>
+
+
                     </div>
+                  </div>
                 </div>
-      </div>
-     
-    </div>
+              </div>
+            </div>
 
-  </div>
-</div>
-​
-​
+          </div>
+
+        </div>
+      </div>
+
+
     </div>
   );
 }
